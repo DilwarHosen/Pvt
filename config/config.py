4942d889262308134f986cf76c148323e8c5a19d
newiseland <herukiraj@gmail.com>
2024-12-05 08:51:21 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/newiseland/UPREPONEW",
+    "https://github.com/newiseland/UPREPONEW2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
