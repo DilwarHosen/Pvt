c9439d444868513b7ac74ad849981859be0ce4d9
newiseland <herukiraj@gmail.com>
2024-12-08 10:14:10 +0530

    Update stats.py

--- VIPMUSIC/plugins/tools/stats.py
+++ VIPMUSIC/plugins/tools/stats.py
@@ -361,8 +361,8 @@ async def overall_stats(client, CallbackQuery, _):
 **sᴛᴏʀᴀɢᴇ ᴜsᴇᴅ:** {used[:4]} ɢiʙ
 **sᴛᴏʀᴀɢᴇ ʟᴇғᴛ:** {free[:4]} ɢiʙ
 
-**sᴇʀᴠᴇᴅ ᴄʜᴀᴛs:** {3347} 
-**sᴇʀᴠᴇᴅ ᴜsᴇʀs:** {5412} 
+**sᴇʀᴠᴇᴅ ᴄʜᴀᴛs:** {served_chats} 
+**sᴇʀᴠᴇᴅ ᴜsᴇʀs:** {served_users} 
 **ʙʟᴏᴄᴋᴇᴅ ᴜsᴇʀs:** {blocked} 
 **sᴜᴅᴏ ᴜsᴇʀs:** {sudoers} 
 
