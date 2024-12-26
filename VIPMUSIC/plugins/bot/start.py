e81be61a22aa9777f74997a18624027c8ff2b8d7
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2024-12-26 16:26:50 +0530

    Add files via upload

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -305,7 +305,7 @@ async def welcome(client, message: Message):
                 out = start_pannel(_)
                 await message.reply_photo(
                     photo=photo_file,
-                    caption=_["start_2"],
+                    caption=_["alone_1"],
                     reply_markup=InlineKeyboardMarkup(out),
                 )
             if member.id in config.OWNER_ID:
